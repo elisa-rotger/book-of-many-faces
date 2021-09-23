db8b43d1a766029f041cc350c6f1258417193477
Elisa Rotger <elisar021@gmail.com>
2021-09-23 20:57:28 +0200

    gets api calls working, general get working

--- my-express-app/routes/users.js
+++ my-express-app/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Hello hello!');
 });
+
+router.get("/mvp", (req, res) => {
+    db(`SELECT * FROM npcs ORDER BY id ASC;`)
+        .then(results => {
+            res.send(results.data)
+        })
+        .catch(err => res.status(500).send({ error: err.message }))
+})
+
+
 
 module.exports = router;
