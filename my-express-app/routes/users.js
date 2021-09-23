0a4195a5f98a06c5871be44ac3bb50ab45b03389
Elisa Rotger <elisar021@gmail.com>
2021-09-23 21:00:49 +0200

    implements a helper function

--- my-express-app/routes/users.js
+++ my-express-app/routes/users.js
@@ -7,12 +7,17 @@ router.get('/', function(req, res, next) {
   res.send('Hello hello!');
 });
 
+//general helper function to make life easier
+function getNPCs(req, res) {
+  db(`SELECT * FROM npcs ORDER BY id ASC;`)
+  .then(results => {
+      res.send(results.data)
+  })
+  .catch(err => res.status(500).send({ error: err.message }))
+}
+
 router.get("/mvp", (req, res) => {
-    db(`SELECT * FROM npcs ORDER BY id ASC;`)
-        .then(results => {
-            res.send(results.data)
-        })
-        .catch(err => res.status(500).send({ error: err.message }))
+  getNPCs(req, res);
 })
 
 
