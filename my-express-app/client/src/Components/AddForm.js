527f46aefe228223fb2a9d44709929a3607e5702
Elisa Rotger <elisar021@gmail.com>
2021-09-29 21:44:37 +0200

    installs bootstrap-react and modifies the addnpc form into an accordion

--- my-express-app/client/src/Components/AddForm.js
+++ my-express-app/client/src/Components/AddForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./AddForm.css";
+import { Accordion } from "react-bootstrap";
 
 export default function AddForm(props) {
     //empty state object
@@ -38,87 +39,94 @@ export default function AddForm(props) {
     }
 
     return (
-        <form>
-            <div className="container" id="npc-form">
-                <div className="item">
-                    <label> first name <br />
-                        <input 
-                        type="text"
-                        name="firstname"
-                        value={npc.firstname} 
-                        onChange={handleChange}/>
-                    </label> <br /><br />
-                    <label> age <br />
-                        <input 
-                        type="text"
-                        name="age"
-                        value={npc.age}
-                        onChange={handleChange}/>
-                    </label>
-                </div>
-                <div className="item">
-                    <label> last name <br />
-                        <input 
-                        type ="text"
-                        name="lastname"
-                        value={npc.lastname}
-                        onChange={handleChange}/>
-                    </label> <br /><br />
-                    <label> race <br />
-                        <input 
-                        type ="text"
-                        name="race"
-                        value={npc.race}
-                        onChange={handleChange}/>
-                    </label>
-                </div>
-                <label className="item" id="description">description <br />
-                    <textarea 
-                    name="description"
-                    value={npc.description}
-                    onChange={handleChange}/>
-                </label>
-                <div className="item">
-                    <label> class <br />
-                        <input 
-                        type ="text"
-                        name="class"
-                        value={npc.class}
-                        onChange={handleChange}/>
-                    </label><br /><br />
-                    <label> residence <br />
-                        <input 
-                        type ="text"
-                        name="residence"
-                        value={npc.residence}
-                        onChange={handleChange}/>
-                    </label>
-                </div>
-                <div className="item">
-                    <label>gender <br />
-                        <select className="form-select" name="gender" value={npc.gender} onChange={handleChange}>
-                            <option />
-                            <option value="Female"> Female </option>
-                            <option value="Male"> Male </option>
-                            <option value="Other"> Other </option>
-                        </select>
-                    </label> <br /> <br />
-                    <label>image <br />
-                        <input  
-                        type ="text"
-                        name="image"
-                        value={npc.image}
-                        onChange={handleChange} />
-                    </label>
-                </div>
-                <label className="item" id="notes">notes <br />
-                    <textarea
-                    name="notes"
-                    value={npc.notes}
-                    onChange={handleChange}/>
-                </label>
-            </div>
-            <button type="button" onClick={handleSubmit}>create npc</button>
-        </form>
+        <Accordion defaultActiveKey="0" flush>
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>c reate new NPC</Accordion.Header>
+                <Accordion.Body>
+                    <form>
+                        <div className="container" id="npc-form">
+                            <div className="item">
+                                <label> first name <br />
+                                    <input 
+                                    type="text"
+                                    name="firstname"
+                                    value={npc.firstname} 
+                                    onChange={handleChange}/>
+                                </label> <br /><br />
+                                <label> age <br />
+                                    <input 
+                                    type="text"
+                                    name="age"
+                                    value={npc.age}
+                                    onChange={handleChange}/>
+                                </label>
+                            </div>
+                            <div className="item">
+                                <label> last name <br />
+                                    <input 
+                                    type ="text"
+                                    name="lastname"
+                                    value={npc.lastname}
+                                    onChange={handleChange}/>
+                                </label> <br /><br />
+                                <label> race <br />
+                                    <input 
+                                    type ="text"
+                                    name="race"
+                                    value={npc.race}
+                                    onChange={handleChange}/>
+                                </label>
+                            </div>
+                            <label className="item" id="description">description <br />
+                                <textarea 
+                                name="description"
+                                value={npc.description}
+                                onChange={handleChange}/>
+                            </label>
+                            <div className="item">
+                                <label> class <br />
+                                    <input 
+                                    type ="text"
+                                    name="class"
+                                    value={npc.class}
+                                    onChange={handleChange}/>
+                                </label><br /><br />
+                                <label> residence <br />
+                                    <input 
+                                    type ="text"
+                                    name="residence"
+                                    value={npc.residence}
+                                    onChange={handleChange}/>
+                                </label>
+                            </div>
+                            <div className="item">
+                                <label>gender <br />
+                                    <select className="form-select" name="gender" value={npc.gender} onChange={handleChange}>
+                                        <option />
+                                        <option value="Female"> Female </option>
+                                        <option value="Male"> Male </option>
+                                        <option value="Other"> Other </option>
+                                    </select>
+                                </label> <br /> <br />
+                                <label>image <br />
+                                    <input  
+                                    type ="text"
+                                    name="image"
+                                    value={npc.image}
+                                    onChange={handleChange} />
+                                </label>
+                            </div>
+                            <label className="item" id="notes">notes <br />
+                                <textarea
+                                name="notes"
+                                value={npc.notes}
+                                onChange={handleChange}/>
+                            </label>
+                        </div>
+                        <button type="button" onClick={handleSubmit}>create npc</button>
+                    </form>
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
     )
 }
