44c50b261d9e7b9cae5a7302ae61078853fdc014
Elisa Rotger <elisar021@gmail.com>
2021-09-30 20:12:41 +0200

    search bar functional

--- my-express-app/client/src/Components/Portfolio.js
+++ my-express-app/client/src/Components/Portfolio.js
@@ -1,17 +1,37 @@
+import React, { useState } from "react";
 import "./Portfolio.css";
 
 export default function Portfolio(props) {
+    const [search, setSearch] = useState("");
+
+    const filterNPCS = (npcs, search) => {
+        if(!search) { return npcs }
+        return npcs.filter((npc) => {
+            const name = npc.firstname.toLowerCase() + npc.lastname.toLowerCase();
+            return name.includes(search)
+        })
+    }
+
+    const filteredNPCS = filterNPCS(props.npcs, search)
+
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+    }
+
     return (
     <div className="wrap">
         <div className="search-bar">
             <span>search:</span>
-            {/* TODO */}
-            <input />
+            <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={handleChange} />
         </div>
         <div className="tab-content">
         <ul className="tiles">
             {props.npcs &&
-            props.npcs.filter(n => n.game_id === props.currentGame).map(n => (
+            filteredNPCS.filter(n => n.game_id === props.currentGame).map(n => (
                 <li key={n.id} className="mod-tile">
                     <div className="mod-image">
                         <img src={n.image} alt="description of the character"/>
