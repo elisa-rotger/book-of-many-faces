a3ea9861e567b16a866f57068eb0af56981d76eb
Elisa Rotger <elisar021@gmail.com>
2021-10-01 16:21:26 +0200

    adds a non functional edit button on the popup

--- my-express-app/client/src/Components/PopUp.js
+++ my-express-app/client/src/Components/PopUp.js
@@ -13,6 +13,8 @@ const PopUp = (props) => {
                 <div className="box-content">
                     <img src={props.npc.image} />
                     <div className="info">
+                        {/* TODO */}
+                        <button className="edit-btn">edit NPC</button>
                         <span id="name">{props.npc.firstname} {props.npc.lastname}</span>
                         <span>{props.npc.race} {props.npc.class}, {props.npc.age} years old</span>
                         <span>Lives in: {props.npc.residence}</span>
