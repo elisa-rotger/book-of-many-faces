78c6c5fac54342d10ce8f91013b52190f40b0812
Elisa Rotger <elisar021@gmail.com>
2021-10-05 17:41:03 +0200

    fixes the nested dropdown for every npc

--- my-express-app/client/src/Components/Portfolio.js
+++ my-express-app/client/src/Components/Portfolio.js
@@ -93,57 +93,66 @@ export default function Portfolio(props) {
             value={search}
             onChange={handleChange} />
         </div>
+
         <div className="tab-content">
-        
-        <ul className="tiles">
-            {props.npcs &&
-            filteredNPCS.filter(n => n.game_id === props.currentGame).map(n => (
-                <li key={n.id} className="mod-tile">
-                    <div className="dropdown" id="droptile">
-                        <button className="dropbtn" type="button" id="tile-btn"> + </button>
-                        <div className="dropdown-content" id="drop-content">
-                            {/* TODO */}
-                            <a>Favourite</a>
-                            <a onClick={() => handleDelete(n.id)}>Delete</a>
-                            <a>Kill</a>
-                            <a>Send to...</a>
+            <ul className="tiles">
+                {props.npcs &&
+                filteredNPCS.filter(n => n.game_id === props.currentGame).map(n => (
+                    <li key={n.id} className="mod-tile">
+                        <div className="dropdown" id="droptile">
+                            <button className="dropbtn" type="button" id="tile-btn"> + </button>
+                            <div className="dropdown-content" id="drop-content">
+                                {/* TODO */}
+                                <a className="dropdown-item">Favourite</a>
+                                <a className="dropdown-item">Kill</a>
+                                <a className="dropdown-item dropdown" id="send-drop">
+                                    <button type="button" className="btn">
+                                    Send to...
+                                    </button>
+                                    <div className="dropdown-content" id="send-content">
+                                        {folders && folders.map((f) => (
+                                            <a className="dropdown-item" key={f.id}>{f.folder}</a>
+                                        ))}
+                                    </div>
+                                </a>
+                                <a className="dropdown-item btn-outline-danger" id="delete-btn" onClick={() => handleDelete(n.id)}>Delete</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="mod-image">
-                        <img src={n.image} alt="description of the character" onClick={() => openPopup(n.id)}/>
-                    </div>
-                    <div className="tile-content">
-                        <p>{n.firstname} {n.lastname}</p>
-                    </div>
-                </li>
-            ))}
-            {folders && 
-            folders.map(f => (
-                <li key={f.id} className="mod-tile">
-                    <div className="mod-image">
-                        <img src={f.image} alt="new folder"/>
-                    </div>
-                    {f.folder === "folder" ? 
-                    <div className="tile-content">
-                        <form>
-                            <input 
-                            type="text" 
-                            placeholder="new folder"
-                            name="folder"
-                            value={folder}
-                            onChange={e => setFolder(e.target.value)} 
-                            />
-                            <button type="button" onClick={() => updateFolder(folder, f.id)}>update</button>
-                         </form>
-                    </div>
-                    : 
-                    <div>
-                        <p>{f.folder}</p>
-                    </div>}
-                </li>
-            ))}
-        </ul>
-        {isOpen && <PopUp handleClose={closePopup} npc={featured} updateNPC={(updatedNPC) => updateNPC(updatedNPC)} />}
+                        <div className="mod-image">
+                            <img src={n.image} alt="description of the character" onClick={() => openPopup(n.id)}/>
+                        </div>
+                        <div className="tile-content">
+                            <p>{n.firstname} {n.lastname}</p>
+                        </div>
+                    </li>
+                ))}
+                {folders && 
+                folders.map(f => (
+                    <li key={f.id} className="mod-tile">
+                        <div className="mod-image">
+                            <img src={f.image} alt="new folder"/>
+                        </div>
+                        {f.folder === "folder" ? 
+                        <div className="tile-content">
+                            <form>
+                                <input 
+                                type="text" 
+                                placeholder="new folder"
+                                name="folder"
+                                value={folder}
+                                onChange={e => setFolder(e.target.value)} 
+                                />
+                                <button type="button" onClick={() => updateFolder(folder, f.id)}>update</button>
+                            </form>
+                        </div>
+                        : 
+                        <div>
+                            <p>{f.folder}</p>
+                        </div>}
+                    </li>
+                ))}
+            </ul>
+            {isOpen && <PopUp handleClose={closePopup} npc={featured} updateNPC={(updatedNPC) => updateNPC(updatedNPC)} />}
         </div>
     </div>
     )
